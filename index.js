032f118104461c6af8240f229cf906887dd121c2
Fantasy Shao <forever.fantasy27@gmail.com>
2015-12-10 23:28:27 +0800

    v0.2.0

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// Module dependencies
-var React = require('react-native');
+'use strict';
 
-var {
+// Module dependencies
+let React = require('react-native');
+let styles = require('./styles');
+
+let {
   Text,
   View,
   TouchableOpacity,
   PropTypes
 } = React;
+
+const STATUS_BAR_HEIGHT = 20;
 
 module.exports = React.createClass({
 
@@ -16,17 +21,23 @@ module.exports = React.createClass({
     tintColor: PropTypes.string,
     titleTextColor: PropTypes.string,
     barTintColor: PropTypes.string,
+    actionName: PropTypes.string,
+    actionFunc: PropTypes.func,
+    backIconHidden: PropTypes.bool,
     statusbarPadding: PropTypes.bool
   },
 
   getDefaultProps () {
     return {
-      title: '标题',
+      title: 'title',
       backFunc () { console.log('press back'); },
       tintColor: '#777',
       titleTextColor: '#333',
       barTintColor: 'white',
-      statusbarPadding: false
+      actionName: '',
+      actionFunc () {},
+      backIconHidden: false,
+      statusbarPadding: true
     }
   },
 
@@ -34,48 +45,17 @@ module.exports = React.createClass({
     return (
       <View style={
           [styles.navbar,
-            {backgroundColor:this.props.barTintColor},
-            this.props.statusbarPadding ? {paddingTop: 30}:{}]}>
+            {backgroundColor:this.props.barTintColor,height: this.props.statusbarPadding ? 59 : 39},
+            this.props.statusbarPadding ? { paddingTop: STATUS_BAR_HEIGHT }:{}]}>
         <TouchableOpacity style={styles.iconWrapper} onPress={this.props.backFunc}>
           <View style={[styles.icon, {borderColor:this.props.tintColor}]} />
         </TouchableOpacity>
         <Text style={[styles.title, {color:this.props.titleTextColor}]}>{this.props.title}</Text>
+        {this.props.actionName ?
+          <TouchableOpacity style={styles.actionBtn} onPress={this.props.actionFunc.bind(this)}>
+            <Text style={styles.actionName}>{this.props.actionName}</Text>
+          </TouchableOpacity> : null}
       </View>
     );
-  }
-});
-
-var styles = React.StyleSheet.create({
-  navbar: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: '#d4d4d4',
-    borderBottomWidth: 0.5,
-    height: 70
-  },
-
-  iconWrapper: {
-    width: 30,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    left: 10,
-    bottom: 8
-  },
-
-  icon: {
-    width: 14,
-    height: 14,
-    borderLeftWidth: 2,
-    borderBottomWidth: 2,
-    transform: [{rotate:'45deg'}]
-  },
-
-  title: {
-    fontSize: 18,
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center'
   }
 });
---
+++ styles.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let React = require('react-native');
+
+const HEIGHT = 39;
+
+module.exports = React.StyleSheet.create({
+  navbar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#d4d4d4',
+    borderBottomWidth: 0.5,
+    height: HEIGHT
+  },
+
+  iconWrapper: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 10,
+    bottom: 6
+  },
+
+  icon: {
+    width: 14,
+    height: 14,
+    borderColor: '#666',
+    borderLeftWidth: 2,
+    borderBottomWidth: 2,
+    transform: [{rotate:'45deg'}]
+  },
+
+  title: {
+    fontSize: 18,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  actionBtn: {
+    position: 'absolute',
+    right: 10,
+    bottom: 12
+  },
+
+  actionName: {
+    fontSize: 14,
+    color: '#666'
+  }
+});
